a830c21459ffd31871ded19cb8aaa4e7aa79a5a1
thowhidulislam <thowhid.ndc14@gmail.com>
2022-03-11 12:50:18 +0600

    showing incorrect reported post error fixed

--- js/app.js
+++ js/app.js
@@ -152,6 +152,7 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  document.getElementById('reported').textContent = '';
   const reportedPosts = getReportedPosts();
   reportedPosts.forEach((post) => {
     const div = createPost(post);
